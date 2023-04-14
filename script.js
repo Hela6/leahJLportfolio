47f8bf0b7e7d5d2bb6d70e49c2309abf4744ca3c
Leah <leah.jl@proton.me>
2023-04-14 17:57:41 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+const hamburgerToggler = document.querySelector(".hamburger");
+const navLinksContainer = document.querySelector(".navlinks_container");
+
+
+function toggleNav() {
+    hamburgerToggler.classList.toggle("open");
+
+    navLinksContainer.classList.toggle("open");
+}
